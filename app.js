8cd81adf7f24c9891c91e0da88c384c1ed27f4c9
Saravanan21-dev <saravanan.learning21@gmail.com>
2025-12-28 11:38:05 +0530

    Update API message response text

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.get('/health', (req, res) => {
 // Sample API endpoint
 app.get('/api/message', (req, res) => {
   res.json({
-    message: 'Hello from Node.js API',
+    message: 'Hello bro from Node.js API',
     version: 'v1'
   });
 });
